3c55ea6bad2dc3a52bf8f50ef987a1b81217dfe3
Marte <125874444+mar-ols@users.noreply.github.com>
2024-05-01 19:06:00 +0200

    Removes link to create new listing if user isn't logged in

--- src/js/functions/listings/displayListings.js
+++ src/js/functions/listings/displayListings.js
@@ -1,6 +1,7 @@
 import { getListings } from "../../api/calls/listings/read.js";
 import { listingsIndex } from "../../api/constants.js";
 import { listingCard } from "../display-templates/listingCard.js";
+import { loadStorage } from "../storage/localStorage.js";
 import { errorMsg } from "../error.js";
 import { loader } from "../loader.js";
 
@@ -11,6 +12,12 @@ export async function displayListings() {
   loader();
   try {
     const fetchListings = await getListings(listingsIndex);
+    const profile = loadStorage("profile");
+    const createNewListing = document.querySelector(".newListingH2");
+
+    if (!profile) {
+      createNewListing.remove();
+    }
 
     const listingContainer = document.querySelector(".listings");
     listingContainer.innerText = "";
