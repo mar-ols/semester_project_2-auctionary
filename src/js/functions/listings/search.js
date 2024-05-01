3f72dfac06f90d3ff2389b85add54245b03a6709
Marte <125874444+mar-ols@users.noreply.github.com>
2024-05-01 19:57:03 +0200

    Fixes broken search

--- src/js/functions/listings/search.js
+++ src/js/functions/listings/search.js
@@ -10,15 +10,16 @@ import { getListings } from "../../api/calls/listings/read.js";
 import { listingCard } from "../display-templates/listingCard.js";
 
 export async function search(value) {
-  const searchListingsURL = `${API_BASE}${API_LISTINGS}?${API_SEARCH}title&${API_SELLER}&${API_QUERY_BIDS}&${API_SORT}`;
+  const searchListingsURL = `${API_BASE}${API_LISTINGS}?${API_SEARCH}${value}`;
   const searchResults = await getListings(searchListingsURL);
+  console.log(searchListingsURL);
 
   const listingContainer = document.querySelector(".listings");
   listingContainer.innerText = "";
 
   searchResults.data.forEach((listing) => {
-    if (listing.title.includes(value) || listing.description.includes(value)) {
-      if (listing.description && listing.media[0]) {
+    if (listing.description && listing.description.includes(value)) {
+      if (listing.media[0]) {
         listingContainer.append(listingCard(listing));
       }
     }
