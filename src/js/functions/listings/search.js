ffabf6eaa8c0658a8aa09befe932d783b879a309
Marte <125874444+mar-ols@users.noreply.github.com>
2024-05-15 13:10:03 +0200

    Fixes wrong search API link

--- src/js/functions/listings/search.js
+++ src/js/functions/listings/search.js
@@ -3,14 +3,14 @@ import { getListings } from "../../api/calls/listings/read.js";
 import { listingCard } from "../display-templates/listingCard.js";
 
 export async function search(value) {
-  const searchListingsURL = `${API_BASE}${API_LISTINGS}?${API_SEARCH}${value}`;
+  const searchListingsURL = `${API_BASE}${API_LISTINGS}${API_SEARCH}${value}`;
   const searchResults = await getListings(searchListingsURL);
 
   const listingContainer = document.querySelector(".listings");
   listingContainer.innerText = "";
 
   searchResults.data.forEach((listing) => {
-    if (listing.description && listing.description.includes(value)) {
+    if (listing.description) {
       if (listing.media[0]) {
         listingContainer.append(listingCard(listing));
       }
